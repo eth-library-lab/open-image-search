d5c80e1058af075a494b8e9f728e8f2c75726f95
barry <barrysunderland@outlook.com>
2021-04-05 14:09:14 +0200

    change datamodel to match graph-samm export

--- backend/api/ImageSearch/models.py
+++ backend/api/ImageSearch/models.py
@@ -4,9 +4,15 @@ from django.db import models
 
 class ImageMetadata(models.Model):
     
-    object_id = models.IntegerField(blank=False, null=False)
-    created_date = models.DateTimeField(auto_now=True)
+    record_id = models.IntegerField(blank=False, null=False)
+    created_date = models.DateTimeField("db_created_date",auto_now=True)
     title = models.CharField(blank=True, null=True, max_length=200)
     image_url = models.URLField(blank=False, null=False, max_length=200)
-    detail_url = models.URLField(blank=False, null=False, max_length=200)
-    detail_description = models.CharField(blank=True, null=True, max_length=200)
+    record_url = models.URLField(blank=False, null=False, max_length=200)
+    inventory_number = models.CharField(blank=True, null=True, max_length=50)
+    person = models.CharField(_("artist who produced work"), max_length=200)
+    date = models.CharField(_("data of the work"), blank=True, null=True, max_length=200)
+    classification =  models.CharField(_("type of work"), blank=True, null=True, max_length=200)
+    material_technique = models.CharField(_("techniques used"), blank=True, null=True, max_length=200)
+    institution_isil = models.CharField(_("credit line"), blank=True, null=True, max_length=50)
+    image_licence = models.CharField(_("image licence"), blank=True, null=True, max_length=50)
