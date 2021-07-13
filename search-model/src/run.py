170696459d06d0c854d742eb75dde4313e53210c
barry <barrysunderland@outlook.com>
2021-07-13 16:50:47 +0200

    add main function to run.py

--- search-model/src/run.py
+++ search-model/src/run.py
@@ -3,11 +3,19 @@ import calculate_features
 import train_similarity_search
 import settings
 
-# preprocess images before calculating features
-if settings.preprocess_images and (settings.raw_image_dir != settings.processed_image_dir):
-    preprocess_images.main()
+def main():
 
-# calculate features from images in in the data/processed directory ( settings.processed_image_dir)
-calculate_features.main()
-# make KNN model
-train_similarity_search.main()
+    # preprocess images before calculating features
+    if settings.preprocess_images and (settings.raw_image_dir != settings.processed_image_dir):
+        preprocess_images.main()
+
+    # calculate features from images in in the data/processed directory ( settings.processed_image_dir)
+    calculate_features.main()
+    # make KNN model
+    train_similarity_search.main()
+
+    return 1
+
+if __name__ == "__main__":
+
+    main()
