311dd5d9714b6a25cab4c9a8b90b47b0f5c8a607
mwang625 <mengqi.wang@uzh.ch>
2021-09-14 15:49:07 +0200

    add plot sideByside to model comparision notebook

--- search-model/notebooks/notebook_utils.py
+++ search-model/notebooks/notebook_utils.py
@@ -5,12 +5,15 @@ from PIL import Image
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from textwrap import wrap
 
 
 def get_model_id_list(df, record_id_list):
     model_id_lst = []
     for record in record_id_list:
         model_id_idx = df.loc[df['record_id']==record].index.tolist() #135484
+        if len(model_id_idx) == 0:
+            print('RecordID not found: ',record)
         model_id_lst = model_id_lst + model_id_idx
     return model_id_lst
 
@@ -23,6 +26,18 @@ def plot_query_imgs(df, model_id_lst):
         plt.imshow(img)
         plt.show()
 
+def clean_record_list(df, record_id_list):
+    #for record in record_id_list:
+        #result = df.loc[df['record_id']==record]
+        #if record in [282489,282491]:
+        #    print(result)
+        #if len(result) == 0:
+        #if record not in df['record_id']:
+        #    record_id_list.remove(record)
+    #print([int(x) in df['record_id'].values for x in record_id_list if x in [20655,17537,2453]])
+    new_record_id_list = [x for x in record_id_list if x in df['record_id'].values]
+    return new_record_id_list
+        
 def get_record_ids_from_features_df(df_feat):
     """
     using the df from the calculated features csv
@@ -165,3 +180,57 @@ def plot_realted_images(record_id, df, show_plots=True, save_figures=False):
 
     _ = [print(str(i+1), ': ', row['recordURL'])
          for i, (idx, row) in enumerate(true_metadata.iterrows())]
+
+
+def plot_similar_images_in_pairs(record_id, results1, results2, show_plots=True, save_figures=False):
+    """
+    record_id: list of record IDs of related artworks
+    df: df with image metadata
+    """
+
+    # plot results
+    num_plots = min(len(results1), 20)
+    fig, axs = plt.subplots(nrows=num_plots, ncols=2,
+                            figsize=(8, int(num_plots*4)))
+
+    # show similar images
+    #for i, (idx, row) in enumerate(results_list1.iterrows()):
+    for i, ((_,row1),(_,row2)) in enumerate(zip(results1.iterrows(),results2.iterrows())):
+
+        axs[i,0].set_title("\n".join(wrap(str(i+1) + ': ' + row1['title'],35)))#, wrap=True
+        axs[i,1].set_title("\n".join(wrap(str(i+1) + ': ' + row2['title'],45)))
+
+        im1 = plt.imread(row1['fpath'])
+        im2 = plt.imread(row2['fpath'])
+        # axs[i+1].axis('off')
+        axs[i,0].imshow(im1)
+        axs[i,1].imshow(im2)
+
+    for ax_r in axs:
+        for ax_c in ax_r:
+            ax_c.set_xticks([])
+            ax_c.set_yticks([])
+
+    for i in range(2):
+        for sp_name in axs[0,i].spines:
+
+            axs[0,i].spines[sp_name].set_visible(True)
+            axs[0,i].spines[sp_name].set_linewidth(3)
+
+
+    fig.tight_layout()
+    if show_plots:
+        plt.show()
+
+    if save_figures:
+        fldr = os.path.join("..", "data", "interim", "test_images")
+        if not os.path.exists(fldr):
+            os.makedirs(fldr)
+
+        fig_fname = '{}: {}.png'.format(record_id, results1['title'])
+        fig_fpath = os.path.join(fldr, fig_fname)
+        fig.savefig(fig_fpath)
+    '''
+    _ = [print(str(i+1), ': ', row['recordURL'])
+         for i, (idx, row) in enumerate(results1.iterrows())]
+    '''    
