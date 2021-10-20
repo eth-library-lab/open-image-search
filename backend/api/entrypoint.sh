94b3be98d81970b6e54ab525874b5b9e63776a35
barry <barrysunderland@outlook.com>
2021-10-20 11:43:29 +0200

    remove data loading from api startup into separate script

--- backend/api/entrypoint.sh
+++ backend/api/entrypoint.sh
@@ -3,12 +3,6 @@ sleep 10
 python3 manage.py makemigrations ImageSearch # add each app here to ensure Django sees migrations
 python3 manage.py migrate ImageSearch
 
-python manage.py loaddata ./fixtures/Relationship.json
-python manage.py loaddata ./fixtures/Classification.json
-python manage.py loaddata ./fixtures/MaterialTechnique.json
-python manage.py loaddata ./fixtures/Institution.json
-python manage.py loaddata ./fixtures/ImageMetadata.json
-
 gunicorn --bind=0.0.0.0:8000 --threads=2 --workers=4 settings.wsgi --reload
 # have to bind to 0.0.0.0
 
