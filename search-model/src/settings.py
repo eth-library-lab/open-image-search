8efa33c053f51af55e0d908b4512d297f36ec524
barry <barrysunderland@outlook.com>
2021-12-06 16:47:51 +0100

    updates to settings variable names

--- search-model/src/settings.py
+++ search-model/src/settings.py
@@ -3,27 +3,38 @@ import os
 from dotenv import load_dotenv
 import argparse
 
-if os.environ.get("MODE") == "testing":
-    load_dotenv("../.env.testing")
+env_fpath = os.environ.get("ENV_FILEPATH", None)
+
+if env_fpath:
     
+    load_dotenv(env_fpath)
+    print(f"settings loaded env file: {env_fpath}") 
+
+elif os.environ.get("MODE") == "testing":
+    load_dotenv("../.env.testing")
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+# set the path to the default data directory (holding the raw, interim and processed data) 
+DATA_DIR = os.path.join(BASE_DIR,"data")
+print(f"DATA_DIR: {DATA_DIR}")
 # name for the collection of images. e.g. the instition's name 
-dataset_name = os.environ.get("DATASET_NAME","test_set")
+dataset_name = os.environ.get("DATASET_NAME","nms")
 
 #### File Input and Output Settings ####
 # run the preprocessing images pipeline to reseize,filter and save the raw images
-preprocess_images = int(os.environ.get('PREPROCESS_IMAGES', 1))
+preprocess_images = int(os.environ.get('PREPROCESS_IMAGES', 0))
 
 ###################################################################################################
 #### File Input Settings ####
 
 # provide path to directory of images OR path to a dataframe with filepath and identifier columns
 #    directory of input images
-raw_image_dir = os.environ.get('INPUT_IMAGE_DIR', f"data/raw/{dataset_name}/images")
-raw_image_dir = os.path.join(BASE_DIR, raw_image_dir)
+raw_image_dir = os.environ.get('INPUT_IMAGE_DIR', None)
+if not raw_image_dir:
+    raw_image_dir = os.path.join(DATA_DIR, f"raw/{dataset_name}/images")
 
 # (alternative) csv file that has a list of file paths to the image files
 files_csv_fpath=None
@@ -34,30 +45,32 @@ label_col_name='id' # name of the column in the csv with the unique id of the im
 metadata_csvs = os.environ.get("METADATA_CSVS", None)
 if metadata_csvs:
     metadata_csvs = metadata_csvs.split(" ")
-    metadata_csvs = [os.path.join(BASE_DIR, fpath) for fpath in metadata_csvs]
+    metadata_csvs = [os.path.join(DATA_DIR, fpath) for fpath in metadata_csvs]
 
 # directory of images to filter from the raw images set 
 # (e.g. the image dataset contains a 'file not found' placeholder image)
-removal_image_dir = os.environ.get("REMOVAL_IMAGE_DIR", f"data/raw/{dataset_name}/images_to_remove")
-removal_image_dir = os.path.join(BASE_DIR, removal_image_dir)
+removal_image_dir = os.environ.get("REMOVAL_IMAGE_DIR", None)
+if not removal_image_dir:
+    removal_image_dir = os.path.join(DATA_DIR, f"raw/{dataset_name}/images_to_remove")
 
 ###################################################################################################
 #### File Output Settings ####
 
 # directory of images to use to calculate features
-processed_image_dir = os.environ.get("OUTPUT_IMAGE_DIR", f"data/processed/{dataset_name}/images")
-processed_image_dir = os.path.join(BASE_DIR, processed_image_dir)
+processed_image_dir = os.environ.get("PREPROCESSED_IMAGE_DIR", None)
+if not processed_image_dir:
+    processed_image_dir = os.path.join(DATA_DIR, f"interim/{dataset_name}/images")
 
 
 # output file for features
-interim_features_fpath = os.path.join(BASE_DIR, 'data','interim', dataset_name,'features','features.csv')
-processed_features_fpath = os.path.join(BASE_DIR, 'data','processed', dataset_name,'features','features.csv')
+interim_features_fpath = os.path.join(DATA_DIR,'interim', dataset_name,'features','features.csv')
+processed_features_fpath = os.path.join(DATA_DIR,'processed', dataset_name,'features','features.csv')
 
 # interim output folder for cleaned metadata files
-interim_metadata_dir = os.path.join(BASE_DIR, 'data','interim', dataset_name, 'metadata')
-processed_metadata_dir = os.path.join(BASE_DIR, 'data','processed', dataset_name, 'metadata')
+interim_metadata_dir = os.path.join(DATA_DIR,'interim', dataset_name, 'metadata')
+processed_metadata_dir = os.path.join(DATA_DIR,'processed', dataset_name, 'metadata')
 # output directory for fixtures
-fixtures_dir = os.path.join(BASE_DIR, 'data','processed', dataset_name,'fixtures')
+fixtures_dir = os.path.join(DATA_DIR,'processed', dataset_name,'fixtures')
 
 ###################################################################################################
 #### Feature Extraction Model Parameters ####
@@ -79,22 +92,28 @@ create_retrieval_model = int(os.environ.get("CREATE_RETRIEVAL_MODEL", 1))
 
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser(description='settings file')
     parser.add_argument('--print', action='store_true',
                         help='print out main settings')
+    # parser.add_argument('--dataset', default=None, 
+    #                     help='print out main settings')
     args = parser.parse_args()
+
 
     if args.print:
         print("open imageSearch - Current Settings\n")
         print("DATASET NAME: ", dataset_name)
         print("BASE_DIR: ", BASE_DIR)
         print("\n----inputs----")
+        print(f"input image directory: {raw_image_dir}")
         print("list of metadata csvs: ")
         if metadata_csvs:
             for metadata_csv in metadata_csvs:
                 print("    - ", metadata_csv)
         print("removal_image_dir: ", removal_image_dir)
         print("\n----outputs----")
+        print(f"output image directory: {processed_image_dir}")
         print("interim metadata dir (for cleaned metadata): ", interim_metadata_dir)
         print("fixtures output directory: ", fixtures_dir)
 
