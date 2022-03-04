c8e30b7535151757efefea70d9368dea81f2e04e
barry <barrysunderland@outlook.com>
2022-03-04 10:19:52 +0100

    remove unused Person table for now

--- backend/api/ImageSearch/models.py
+++ backend/api/ImageSearch/models.py
@@ -3,13 +3,6 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 import uuid
 from django.contrib.postgres.fields import ArrayField
 
-class Person(models.Model):
-    name = models.CharField(max_length=200)
-    created_date = models.DateTimeField("db_created_date", auto_now=True)
-
-    def __str__(self):
-        return self.name
-
 
 class Classification(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -49,7 +42,6 @@ class ImageMetadata(models.Model):
     record_url = models.URLField(blank=False, null=False, max_length=300)
     inventory_number = models.CharField(blank=True, null=True, max_length=50)
     person = models.CharField("artist who produced work",blank=True, null=True, max_length=1500)
-    person_id = models.ManyToManyField(Person)
     date = models.CharField("date of the work", blank=True, null=True, max_length=200)
     classification =  models.CharField("type of work", blank=True, null=True, max_length=200)
     classification_id = models.ForeignKey(Classification, blank=True,null=True, on_delete=models.SET_NULL)
